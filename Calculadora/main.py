f31f69b81ab8f503364e9db3bd1b1407fc12d946
Gustavo Silva <gustavospr@hotmail.com>
2021-01-13 20:36:05 -0300

    Criado os botões da interface
    Foram criados os botões da interface para serem adicionadas os comportamentos.

--- Calculadora/main.py
+++ Calculadora/main.py
@@ -1,23 +1,35 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.stacklayout import StackLayout
 import calculos
 
+class MyLayout(Widget):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return MyLayout()
+
+MyApp().run()
+
 #Coleta os valores e operador uma vez
-valor1 = int(input("Digite o primeiro valor: "))
-valor2 = int(input("Digite o segundo valor: "))
-operacao = str(input("Qual a operação?: "))
+#valor1 = int(input("Digite o primeiro valor: "))
+#valor2 = int(input("Digite o segundo valor: "))
+#operacao = str(input("Qual a operação?: "))
 
 #Verifica se é uma soma e a executa
-if operacao == "+":
-    calculos.soma(valor1, valor2)
+#if operacao == "+":
+#    calculos.soma(valor1, valor2)
 
 #Verifica se é uma subtração e a executa
-elif operacao == "-":
-    calculos.subtracao(valor1, valor2)
+#elif operacao == "-":
+#    calculos.subtracao(valor1, valor2)
 
 #Verifica se é uma multiplicação e a executa
-elif operacao == "*":
-    calculos.multiplicacao(valor1, valor2)
+#elif operacao == "*":
+#    calculos.multiplicacao(valor1, valor2)
 
 #Verifica se é uma divisão e a executa
-elif operacao == "/":
-    calculos.divisao(valor1, valor2)
+#elif operacao == "/":
+#    calculos.divisao(valor1, valor2)
 
