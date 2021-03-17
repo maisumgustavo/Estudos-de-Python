78956c3d8d87ec84dde0cc686a7135f5b3c1c3f3
Gustavo Silva <gustavosilva@pop-os.localdomain>
2021-03-17 20:44:24 -0300

    Criação do projeto PuzzleGame

---
+++ PuzzleGame/main.py
@@ -0,0 +1,63 @@
+opcao = ''
+
+print(
+    '\nSejá bem vindo ao Bolinho Quiz!',
+    '\nEste quiz, tem o objetivo de testar seu conhecimento, sobre porque o presidente bolinho deve ser presidente.',
+    ''
+)
+
+print(
+    '\nQual é a maior e mais ambiciosa promessa que o presidente bolinho já fez em seu canal?',
+    '\na) Correr no polo norte de shortinho;', '      ', 'b) Trazer uma copa para o Brasil;',
+    '\nc) Fazer a nintendo lançar um jogo com dublagem em portugês;',  '      ', 'd) Fazer a Rockstar lançar um GTA no Rio de Janeiro.'
+)
+
+opcao = input('\n')
+
+if opcao == 'c)':
+    print('Resposta certa!')
+else:
+    print('Resposta errada.')
+
+
+print(
+    '\nQual o projeto principal do canal Presidente Bolinho?',
+    '\na) Fazer um jogo;', '      ', 'b) Hackear o governo;',
+    '\nc) Conquistar a keyLady com programação;',  '      ', 'd) Fazer um pais digital.'
+)
+
+opcao = input('\n')
+
+if opcao == 'd)':
+    print('Resposta certa!')
+else:
+    print('Resposta errada.')
+
+
+print(
+    '\nQual é o viwer mais participativo do canal? (até a data da criação do quiz)',
+    '\na) Levxyca', '      ', 'b) Erick_dev_',
+    '\nc) Taiirritadotio;',  '      ', 'd) moniquelive'
+)
+
+opcao = input('\n')
+
+if opcao == 'b)':
+    print('Resposta certa!')
+else:
+    print('Resposta errada.')
+
+
+print(
+    '\nQual a linguagem utilizada para o desenvolvimento dos projetos no canal?',
+    '\na) Python', '      ', 'b) C#',
+    '\nc) JS',  '      ', 'd) C++'
+)
+
+opcao = input('\n')
+
+if opcao == 'a)':
+    print('Resposta certa!')
+else:
+    print('Resposta errada.')
+
